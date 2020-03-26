2c99afa03f9ecbfe7a45f6823fa0fff7571af7c4
Kutsu Shita <Kutsu Shita>
2020-03-26 15:07:43 +0100

    Update PointTracker_StreamlabsSystem.py
    Making sure it actually works

--- PointTracker_StreamlabsSystem.py
+++ PointTracker_StreamlabsSystem.py
@@ -14,10 +14,12 @@ Version = "1.0.0.0"
 
 chairman = 'KutsuSheeta'
 lines = []
+Command = '!pt'
 
 def Init():
     with open('Services\Scripts\Channel-Point-Tracker\Tracked.txt') as tracked_file:
         reader = csv.reader(tracked_file)
+        global lines
         lines = list(reader)
     return
 
@@ -26,46 +28,56 @@ def Execute(data):
         return
 
     else:
-        user_name = data.user_name
+        user_name = data.UserName
 
-        if isinstance(data.GetParam(1), int):
-            pt_num = data.GetParam(1)
+        try:
+            int(data.GetParam(1))
+
+        except ValueError:
+            send_message(user_name + ', that\'s not how this command works, you boob! !pt [some number]')
+
+        else:
+            pt_num = int(data.GetParam(1))
 
             if pt_num >= 0 and pt_num < len(lines):
                 pt_name = lines[pt_num][0]
                 pt_desc = lines[pt_num][1]
-                pt_points = lines[pt_num][2]
-                pt_times_met = lines[pt_num][3]
+                pt_points = int(lines[pt_num][2])
+                pt_times_met = int(lines[pt_num][3])
 
-                if isinstance(data.GetParam(2), int) and user_name == chairman:
-                    pt_substract = data.GetParam(2)
-                    pt_points -= pt_substract
+                if data.GetParam(2) and user_name == chairman:
+                    try: 
+                        int(data.GetParam(2))
 
-                    if pt_points <= 0:
-                        send_message('Challenge met! Looks like we get to enjoy "' + pt_name + '"...')
-                        pt_points = 200000
-                        lines[pt_num][3] += 1
-                    
+                    except ValueError:
+                        return
+
                     else:
-                        send_message('"' + pt_name + '" has ' + pt_points + ' points left')
+                        pt_substract = int(data.GetParam(2))
+                        pt_points -= pt_substract
+
+                        if pt_points <= 0:
+                            send_message('Challenge met! Looks like we get to enjoy "' + pt_name + '"...')
+                            pt_points = 200000
+                            pt_times_met += 1
+                            lines[pt_num][3] = pt_times_met
+                    
+                        else:
+                            send_message('"' + pt_name + '" has ' + str(pt_points) + ' points left')
                         
-                    lines[pt_num][2] = pt_points
-                    with open('Services\Scripts\Channel-Point-Tracker\Tracked.txt') as tracked_file:
-                        writer = csv.writer(tracked_file)
-                        writer.writerows(lines)
-                    return
-                
+                        lines[pt_num][2] = pt_points
+                        with open('Services\Scripts\Channel-Point-Tracker\Tracked.txt') as tracked_file:
+                            writer = csv.writer(tracked_file)
+                            writer.writerows(lines)
+                        return
+                    
                 else:
-                    send_message('Challenge: "' + _pt_num + ' - ' + pt_name + '"; ' + pt_desc + ' | Points left: ' + pt_points + ' | Times met: ' + pt_times_met)
+                    send_message('Challenge: ' + str(pt_num) + ' - "' + pt_name + '"; ' + pt_desc + ' | Points left: ' + str(pt_points) + ' | Times met: ' + str(pt_times_met))
                     return
 
             else:
-                send_message('Sorry ' + user_name + ', the number provided (' + pt_num + ') does not have anything associated with it.')
-                return
-
-        else:
-            send_message(user_name + ', that\'s not how this command works, you boob! !pt [some number]')
-            return           
+                send_message('Sorry ' + user_name + ', the number provided (' + str(pt_num) + ') does not have anything associated with it.')
+                return          
 
         if user_name != chairman:
             Parent.AddUserCooldown(ScriptName, Command, data.User, 30)
